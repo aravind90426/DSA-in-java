509212ac2cc5d527b80990fef2060f37d4b82b97
aravind90426 <aravind90426@gmail.com>
2024-10-03 23:19:06 +0530

    new coded

--- LinkedList/SinglyLinkedList/LL.java
+++ LinkedList/SinglyLinkedList/LL.java
@@ -574,4 +574,54 @@ public void reorderList(Node head) {
         hf.next=null;
 
 
-}}
+}
+//https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k <= 1 || head == null) {
+            return head;
+        }
+
+        ListNode current = head;
+        ListNode prev = null;
+
+        int length = getLength(head);
+        int count = length / k;
+        while (count > 0) {
+            Node last = prev;
+            LNode newEnd = current;
+
+            ListNode next = current.next;
+            for (int i = 0; current != null && i < k; i++) {
+                current.next = prev;
+                prev = current;
+                current = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+
+            if (last != null) {
+                last.next = prev;
+            } else {
+                head = prev;
+            }
+
+            newEnd.next = current;
+
+            prev = newEnd;
+            count--;
+        }
+        return head;
+    }
+
+    public int getLength(Node head) {
+        Node node = head;
+        int length = 0;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
+}
